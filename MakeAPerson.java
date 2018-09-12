b2ec76c6d96ecf0e63d555fbe49641c461b77fd1
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-09-12 18:31:33 -0500

    added

--- MakeAPerson.java
+++ MakeAPerson.java
@@ -1,23 +1,13 @@
-import java.util.Scanner;
+package People;
 
 public class MakeAPerson{
 	public static void main(String[] args){
-		Scanner input = new Scanner(System.in);
-		String firstName, lastName, sex;
-		
-		int age;
-		
-       System.out.print("Please, enter your first name: ");
-       firstName = input.nextLine();
-	    System.out.print("Please, enter your last name: ");
-       lastName = input.nextLine();
-	   System.out.print("Please, enter your sex: ");
-       sex = input.nextLine();
-	    System.out.print("Please, enter your age: ");
-       age = input.nextInt();
-	    
-		
-		System.out.println(" I am "+ firstName + " " + lastName + " . \n " + "I am " + age + " years old. " + "I am " + sex + " . ");
-		
+		Person myPerson= new Person();
+		getFirstName();
+		getLastName();
+		getAge();
+		getSex();
+		showInfo();
 	}
+	
 }
---
+++ People/Person.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Person {
+	String firstName, lastName, sex;
+	int age;
+	Scanner inputDevice = new Scanner(System.in);
+	
+	private getFirstName(){
+		firstName = inputDevice.nextLine();
+		System.out.println("Your First Name is : " + firstName);
+		return firstName;
+	}
+	private getLastName(){
+		lastName = inputDevice.nextLine();
+		System.out.println("Your Last name is: " + lastName);
+		return lastName;
+	}
+		public getSex(){
+		sex = inputDevice.nextLine();
+		System.out.println("Your sex is: " + sex);
+		return sex;
+	}
+	private getAge(){
+		age = inputDevice.nextLine();
+		System.out.println("Your age is: " + age);
+		return age;
+	}
+	private static void showInfo(){
+		
+		System.out.println("Your First Name is: " + firstName);
+		System.out.println("Your Last Name is:: " + lastName);
+		System.out.println("Your Age is: " + age);
+		System.out.println("Your Sex is: " + sex);
+	}
+}
